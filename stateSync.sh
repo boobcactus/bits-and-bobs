37aaeaf57c9217bdcc216cb28effbe112690fee5
Olivia <104646754+boobcactus@users.noreply.github.com>
2023-07-22 00:28:07 -0400

    Update stateSync.sh with a comment

--- stateSync.sh
+++ stateSync.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Provide an RPC with port:"
-read SNAP_RPC
+read RPC
 echo "Minus blocks:"
 read MOD
 
-
-CHAIN_ID=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.chain_id); \
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
+CHAIN_ID=$(curl -s $RPC/block | jq -r .result.block.header.chain_id); \
+LATEST_HEIGHT=$(curl -s $RPC/block | jq -r .result.block.header.height); \
 BLOCK_HEIGHT=$((LATEST_HEIGHT - $MOD)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+TRUST_HASH=$(curl -s "$RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
 
+# Chain list, must be updated with find_chain()
 osmosis=".osmosisd"
 stargaze=".starsd"
 evmos=".evmosd"
@@ -45,7 +45,7 @@ find_chain() {
 find_chain
 
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$RPC,$RPC\"| ; \
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
 s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/$COIN/config/config.toml
 
